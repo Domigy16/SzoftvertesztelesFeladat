773c84a8bf8b4fa849bb6a560dd35db8605d4aab
Ádám Erik Dominik <domigy16@gmail.com>
2023-12-18 12:32:24 +0100

    1. feladat létrejött

--- SzoftvertesztelesFeladat/Program.cs
+++ SzoftvertesztelesFeladat/Program.cs
@@ -10,6 +10,15 @@ namespace SzoftvertesztelesFeladat
     {
         static void Main(string[] args)
         {
+            Console.Write("Adjon meg egy számot! ");
+            double szam = Convert.ToDouble(Console.ReadLine());
+            List<double> szamok = new List<double>();
+            while (szam != 0)
+            {
+                szamok.Add(szam);
+                Console.Write("Adjon meg egy számot!");
+                szam = Convert.ToDouble(Console.ReadLine());
+            }
         }
     }
 }
